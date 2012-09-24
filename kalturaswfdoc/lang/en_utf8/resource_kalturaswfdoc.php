f00438a31f25d2b066b47761464278f6bde54bc8
jamesballard <j.ballard@ulcc.ac.uk>
2012-09-24 19:07:58 +0100

    Remove old structure

--- kalturaswfdoc/lang/en_utf8/resource_kalturaswfdoc.php
+++
@@ -1,8 +0,0 @@
-<?php
-$string['resourcetypekalturaswfdoc'] = 'Video Presentation';
-$string['uploaddocument'] = 'Upload Document';
-$string['selectvideo'] = 'Select Video';
-$string['syncpoints'] = 'Sync Keypoints';
-$string['syncdescription'] = 'Once a video and document have been selected and ready, you can save your selection and proceed to sync keypoints';
-$string['editsyncpoints'] = 'Edit sync points';
-?>
